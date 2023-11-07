a45094acafca07faa94dbb6a3b3c7c7e49d4d2ea
Octavio Martinez <omartinez1618@gmail.com>
2023-11-07 00:57:23 -0400

    Agregado provider

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Router from './routes/Router';
+import './index.css'
+import { ProviderContext } from './tools/FireContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router />
+    <ProviderContext>
+      <Router />
+    </ProviderContext>
   </React.StrictMode>
 );
 
